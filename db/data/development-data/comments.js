1fd2e7e8e31dd75b62e30892b46660af9218d095
MattGW <matthew.winfield@live.co.uk>
2021-04-27 13:07:43 +0100

    fix for dev data - comments

--- db/data/development-data/comments.js
+++ db/data/development-data/comments.js
@@ -1,31 +1,31 @@
 module.exports = [
   {
     body: 'I loved this game too!',
-    belongs_to: 'JengARRGGGH',
+    belongs_to: 'JengARRGGGH!',
     created_by: 'happyamy2016',
     votes: 16,
-    created_at: 1511354163389,
+    created_at: 1511354163389
   },
   {
     body: 'My dog loved this game too!',
-    belongs_to: 'Ultimate Werewolf',
+    belongs_to: 'One Night Ultimate Werewolf',
     created_by: 'tickle122',
     votes: 3,
-    created_at: 1610964545410,
+    created_at: 1610964545410
   },
   {
     body: "I didn't know dogs could play games",
-    belongs_to: 'Ultimate Werewolf',
+    belongs_to: 'One Night Ultimate Werewolf',
     created_by: 'weegembump',
     votes: 10,
-    created_at: 1610964588110,
+    created_at: 1610964588110
   },
   {
     body: 'EPIC board game!',
-    belongs_to: 'JengARRGGGH',
+    belongs_to: 'JengARRGGGH!',
     created_by: 'tickle122',
     votes: 16,
-    created_at: 1511354163389,
+    created_at: 1511354163389
   },
   {
     body:
@@ -33,29 +33,29 @@ module.exports = [
     belongs_to: 'Monopoly',
     created_by: 'cooljmessy',
     votes: 13,
-    created_at: 1610965445410,
+    created_at: 1610965445410
   },
   {
     body:
       'Not sure about dogs, but my cat likes to get involved with board games, the boxes are their particular favourite',
-    belongs_to: 'Ultimate Werewolf',
+    belongs_to: 'One Night Ultimate Werewolf',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616874588110,
+    created_at: 1616874588110
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: "Scythe; you're gonna need a bigger table!",
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: "That's just what an evil person would say!",
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -63,7 +63,7 @@ module.exports = [
     belongs_to: 'Build you own tour de Yorkshire',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -71,7 +71,7 @@ module.exports = [
     belongs_to: 'JengARRGGGH!',
     created_by: 'grumpy19',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -79,14 +79,14 @@ module.exports = [
     belongs_to: 'One Night Ultimate Werewolf',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521110,
+    created_at: 1616854521110
   },
   {
     body: 'Aliquip aliqua ad fugiat anim ex elit consectetur ut fugiat ex qui.',
     belongs_to: 'A truly Quacking Game; Quacks of Quedlinburg',
     created_by: 'tickle122',
     votes: 8,
-    created_at: 1616854551110,
+    created_at: 1616854551110
   },
   {
     body:
@@ -94,21 +94,21 @@ module.exports = [
     belongs_to: 'Build you own tour de Yorkshire',
     created_by: 'weegembump',
     votes: 8,
-    created_at: 161684559110,
+    created_at: 161684559110
   },
   {
     body: 'Commodo et non ut aute anim nisi occaecat ea veniam ut ad.',
     belongs_to: "That's just what an evil person would say!",
     created_by: 'jessjelly',
     votes: 15,
-    created_at: 1611313922734,
+    created_at: 1611313922734
   },
   {
     body: 'Ex aliqua irure elit aliqua in veniam ex ut pariatur nulla.',
-    belongs_to: 'Dolor reprehenderit',
+    belongs_to: 'Twister',
     created_by: 'tickle122',
     votes: 1,
-    created_at: 1630010368077,
+    created_at: 1630010368077
   },
   {
     body:
@@ -116,7 +116,7 @@ module.exports = [
     belongs_to: "Settlers of Catan: Don't Settle For Less",
     created_by: 'cooljmessy',
     votes: 2,
-    created_at: 1611316192508,
+    created_at: 1611316192508
   },
   {
     body:
@@ -124,7 +124,7 @@ module.exports = [
     belongs_to: "Settlers of Catan: Don't Settle For Less",
     created_by: 'weegembump',
     votes: 10,
-    created_at: 1611316224771,
+    created_at: 1611316224771
   },
   {
     body:
@@ -132,21 +132,21 @@ module.exports = [
     belongs_to: 'Occaecat consequat officia in quis commodo.',
     created_by: 'grumpy19',
     votes: 7,
-    created_at: 1620010368077,
+    created_at: 1620010368077
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Karma Karma Chameleon',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Karma Karma Chameleon',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -154,7 +154,7 @@ module.exports = [
     belongs_to: 'Karma Karma Chameleon',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -162,7 +162,7 @@ module.exports = [
     belongs_to: 'Karma Karma Chameleon',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -170,7 +170,7 @@ module.exports = [
     belongs_to: 'Karma Karma Chameleon',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521110,
+    created_at: 1616854521110
   },
   {
     body:
@@ -178,7 +178,7 @@ module.exports = [
     belongs_to: 'Super Rhino Hero',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -186,21 +186,21 @@ module.exports = [
     belongs_to: 'Super Rhino Hero',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521110,
+    created_at: 1616854521110
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: "Kerplunk; Don't lose your marbles",
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: "Kerplunk; Don't lose your marbles",
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -208,7 +208,7 @@ module.exports = [
     belongs_to: "Kerplunk; Don't lose your marbles",
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -216,7 +216,7 @@ module.exports = [
     belongs_to: "Scrobble, no that's not a typo",
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -224,21 +224,21 @@ module.exports = [
     belongs_to: "Scrobble, no that's not a typo",
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521110,
+    created_at: 1616854521110
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Ticket To Ride',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Ticket To Ride',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -246,7 +246,7 @@ module.exports = [
     belongs_to: 'Ticket To Ride',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -254,7 +254,7 @@ module.exports = [
     belongs_to: 'Ticket To Ride',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -262,7 +262,7 @@ module.exports = [
     belongs_to: 'Ticket To Ride',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521310,
+    created_at: 1616854521310
   },
   {
     body:
@@ -270,7 +270,7 @@ module.exports = [
     belongs_to: 'Ticket To Ride',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538190,
+    created_at: 1616874538190
   },
   {
     body:
@@ -278,21 +278,21 @@ module.exports = [
     belongs_to: 'Ticket To Ride',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538180,
+    created_at: 1616854538180
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Nova Luna; Freak out in a moonage daydream, oh yeah!',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Nova Luna; Freak out in a moonage daydream, oh yeah!',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -300,7 +300,7 @@ module.exports = [
     belongs_to: 'Nova Luna; Freak out in a moonage daydream, oh yeah!',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -308,7 +308,7 @@ module.exports = [
     belongs_to: 'Reef',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -316,7 +316,7 @@ module.exports = [
     belongs_to: 'Reef',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521310,
+    created_at: 1616854521310
   },
   {
     body:
@@ -324,7 +324,7 @@ module.exports = [
     belongs_to: 'Reef',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538190,
+    created_at: 1616874538190
   },
   {
     body:
@@ -332,21 +332,21 @@ module.exports = [
     belongs_to: 'Reef',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538180,
+    created_at: 1616854538180
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Twister',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Monopoly',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -354,7 +354,7 @@ module.exports = [
     belongs_to: 'Ganz Schon Clever',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -362,7 +362,7 @@ module.exports = [
     belongs_to: 'Ganz Schon Clever',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -370,7 +370,7 @@ module.exports = [
     belongs_to: 'Yahtzee',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521310,
+    created_at: 1616854521310
   },
   {
     body:
@@ -378,7 +378,7 @@ module.exports = [
     belongs_to: 'Yahtzee',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538190,
+    created_at: 1616874538190
   },
   {
     body:
@@ -386,21 +386,21 @@ module.exports = [
     belongs_to: 'Yahtzee',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538180,
+    created_at: 1616854538180
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Escape The Dark Castle',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Escape The Dark Castle',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -408,7 +408,7 @@ module.exports = [
     belongs_to: 'Escape The Dark Sector',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
+    created_at: 1616854536110
   },
   {
     body:
@@ -416,7 +416,7 @@ module.exports = [
     belongs_to: 'Escape The Dark Sector',
     created_by: 'jessjelly',
     votes: 9,
-    created_at: 1616854531110,
+    created_at: 1616854531110
   },
   {
     body:
@@ -424,7 +424,7 @@ module.exports = [
     belongs_to: 'Escape The Dark Sector',
     created_by: 'happyamy2016',
     votes: 10,
-    created_at: 1616854521310,
+    created_at: 1616854521310
   },
   {
     body:
@@ -432,7 +432,7 @@ module.exports = [
     belongs_to: 'Escape The Dark Sector',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538190,
+    created_at: 1616874538190
   },
   {
     body:
@@ -440,21 +440,21 @@ module.exports = [
     belongs_to: 'Escape The Dark Sector',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538180,
+    created_at: 1616854538180
   },
   {
     body: 'Quis duis mollit ad enim deserunt.',
     belongs_to: 'Culture a Love of Agriculture With Agricola',
     created_by: 'jessjelly',
     votes: 3,
-    created_at: 1616874538110,
+    created_at: 1616874538110
   },
   {
     body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
     belongs_to: 'Culture a Love of Agriculture With Agricola',
     created_by: 'cooljmessy',
     votes: 17,
-    created_at: 1616854538110,
+    created_at: 1616854538110
   },
   {
     body:
@@ -462,8 +462,8 @@ module.exports = [
     belongs_to: 'Culture a Love of Agriculture With Agricola',
     created_by: 'weegembump',
     votes: 1,
-    created_at: 1616854536110,
-  },
+    created_at: 1616854536110
+  }
 ];
 
 // Scythe; you're gonna need a bigger table!
