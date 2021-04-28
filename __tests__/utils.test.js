6d8860b294bb0e82abe5e40eb7645d82304dbbe1
Joe Grove <joe.grove63@gmail.com>
2021-04-28 13:29:54 +0100

    in process of implementing formatComments

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,6 +1,7 @@
 const {
   createReviewLookupObj,
-  amendDate
+  amendDate,
+  formatComments
 } = require('../db/utils/data-manipulation');
 
 describe('amendDate', () => {
@@ -32,8 +33,119 @@ comments.belongs to = objectlookup.oneNightultimateWerewolf
 
 */
 describe('createReviewLookupObj', () => {
-  test.only('should return an empty object, when passed an empty array', () => {
+  test('should return an empty object, when passed an empty array', () => {
     const actual = createReviewLookupObj([]);
     expect(actual).toEqual({});
+  });
+  test('return a ref obj when passed array with 1 review', () => {
+    const review = [
+      {
+        review_id: 4,
+        title: 'One Night Ultimate Werewolf',
+        review_body: "We couldn't find the werewolf!",
+        designer: 'Akihisa Okui',
+        review_img_url:
+          'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        votes: 5,
+        category: 'hidden-roles',
+        owner: 'happyamy2016',
+        created_at: '2021-01-18T10:01:41.251Z'
+      }
+    ];
+    const actual = createReviewLookupObj(review);
+    expect(actual).toEqual({ 'One Night Ultimate Werewolf': 4 });
+  });
+  test('should return a ref obj when passed in multiple reviews ', () => {
+    const review = [
+      {
+        review_id: 4,
+        title: 'One Night Ultimate Werewolf',
+        review_body: "We couldn't find the werewolf!",
+        designer: 'Akihisa Okui',
+        review_img_url:
+          'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        votes: 5,
+        category: 'hidden-roles',
+        owner: 'happyamy2016',
+        created_at: '2021-01-18T10:01:41.251Z'
+      },
+      {
+        review_id: 5,
+        title: 'A truly Quacking Game; Quacks of Quedlinburg',
+        review_body:
+          "Ever wish you could try your hand at mixing potions? Quacks of Quedlinburg will have you mixing up a homebrew like no other. Each player buys different ingredients (chips) that are drawn at random to reach the most points, but watch out, you'd better not let your cauldrom explode.",
+        designer: 'Wolfgang Warsch',
+        review_img_url: null,
+        votes: 10,
+        category: 'push-your-luck',
+        owner: 'happyamy2016',
+        created_at: '2021-01-18T10:01:41.251Z'
+      }
+    ];
+    const actual = createReviewLookupObj(review);
+    expect(actual).toEqual({
+      'One Night Ultimate Werewolf': 4,
+      'A truly Quacking Game; Quacks of Quedlinburg': 5
+    });
+  });
+  test('does not mutate the original array ', () => {
+    const review = [
+      {
+        review_id: 4,
+        title: 'One Night Ultimate Werewolf',
+        review_body: "We couldn't find the werewolf!",
+        designer: 'Akihisa Okui',
+        review_img_url:
+          'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        votes: 5,
+        category: 'hidden-roles',
+        owner: 'happyamy2016',
+        created_at: '2021-01-18T10:01:41.251Z'
+      }
+    ];
+    createReviewLookupObj(review);
+    expect(review).toEqual([
+      {
+        review_id: 4,
+        title: 'One Night Ultimate Werewolf',
+        review_body: "We couldn't find the werewolf!",
+        designer: 'Akihisa Okui',
+        review_img_url:
+          'https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        votes: 5,
+        category: 'hidden-roles',
+        owner: 'happyamy2016',
+        created_at: '2021-01-18T10:01:41.251Z'
+      }
+    ]);
+  });
+});
+
+describe('formatComments()', () => {
+  test.only('should return an empty array when passed empty array & lookup object', () => {
+    const actual = formatComments([], {});
+    expect(actual).toEqual([]);
+  });
+  test.only('should return an array with 1 formatted comment when passed and array with 1 comment and a lookup obj', () => {
+    const comment = [
+      {
+        body: 'I loved this game too!',
+        belongs_to: 'JengARRGGGH!',
+        created_by: 'happyamy2016',
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    const lookUp = { 'JengARRGGGH!': 2 };
+    const actual = formatComments(comment, lookUp);
+    expect(actual).toEqual([
+      {
+        body: 'I loved this game too!',
+        review_id: 2,
+        created_by: 'happyamy2016',
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ]);
   });
 });
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -16,8 +16,19 @@ const amendDate = (data) => {
   data.forEach((object) => (object.created_at = new Date(object.created_at)));
 };
 
-const createReviewLookupObj = () => {
-  return {};
+const createReviewLookupObj = (reviews) => {
+  const reviewLookUp = {};
+  if (reviews.length === 0) return reviewLookUp;
+  reviews.forEach((review) => (reviewLookUp[review.title] = review.review_id));
+  return reviewLookUp;
 };
 
-module.exports = { amendDate, createReviewLookupObj };
+const formatComments = (commentData, lookupObj) => {
+  const formattedComment = {};
+  if (commentData.length === 0) return [];
+  formattedComment.review_id = lookupObj['JengARRGGGH!'];
+  console.log(formattedComment);
+  //spread rest of comment key value pairs into formattedComment (not belongs_to)
+};
+
+module.exports = { amendDate, createReviewLookupObj, formatComments };
