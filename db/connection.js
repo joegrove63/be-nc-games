e9302c3d2864eabc7c319c78c907a4b8af54e15f
Joe Grove <joe.grove63@gmail.com>
2021-05-04 17:29:00 +0100

    connected heroku

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,14 @@ const { Pool } = require('pg');
 const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({
+  path: path.resolve(__dirname, `../.env.${ENV}`)
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 const config =
   ENV === 'production'
     ? {
@@ -15,12 +23,4 @@ const config =
       }
     : {};
 
-require('dotenv').config({
-  path: path.resolve(__dirname, `../.env.${ENV}`)
-});
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
-module.exports = new Pool();
+module.exports = new Pool(config);
