e22f1fd3c72722fed3442c72ba406a1df4a9c5a0
Joe Grove <joe.grove63@gmail.com>
2021-05-24 12:04:14 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routers/api.router');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
